ddd428e3fc98f81bfcf5fa6f2f3cda9cd457a726
Pavankumar07s <pawankumar14662693@gmail.com>
2024-08-14 00:05:30 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Header from "./components/Header.jsx";
+import Login from "./components/Login.jsx";
+import SignUp from "./components/SignUp.jsx";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <main>  
+        <SignUp />
+      </main>
+    </>
+  );
+}
+
+export default App;
